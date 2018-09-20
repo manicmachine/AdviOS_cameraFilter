594489b9aa79f8026d64eb33940b18458a3e59ff
Friedl <lfriedl@mac-bec152a-07.cvtc.local>
2018-09-20 14:26:00 -0500

    Tutorial for camera

--- groupProject/ViewController.swift
+++ groupProject/ViewController.swift
@@ -7,19 +7,89 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Variables
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Outlets
+    @IBOutlet weak var PreviewView: UIView!
+    @IBOutlet weak var CaptureImageView: UIImageView!
+
+
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let imageData = photo.fileDataRepresentation() else {
+            return
+        }
+        
+        let image = UIImage(data: imageData)
+        CaptureImageView.image = image
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.captureSession.stopRunning()
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video) else {
+            print("unable to access camera")
+            return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+            
+        }
+        catch let error {
+            print("error")
+        }
+    }
+    
+    @IBAction func didTakePhoto(_ sender: Any) {
+        
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self as! AVCapturePhotoCaptureDelegate)
+        
+    }
+    
+    func setupLivePreview() {
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspect
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        PreviewView.layer.addSublayer(videoPreviewLayer)
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning();
+          
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.PreviewView.bounds
+            }
+            
+        }
+        
+    }
+    
 }
 
