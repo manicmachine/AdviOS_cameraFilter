e1d3e1f6ed76e43f6658ff54d89463d12300842a
Friedl, Luke <lfriedl@lab-ecb152a-07.cvtc.local>
2018-09-20 13:24:15 -0500

    Initial Commit

---
+++ groupProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  groupProject
+//
+//  Created by Friedl, Luke on 9/20/18.
+//  Copyright © 2018 Friedl, Luke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
